2d890f960fbd599335e85fe2f5b399293b79357e
justin leggett <justin@codingrookie.com>
2016-12-05 11:54:52 -0600

    basic styling is working on readbible.html. I also added images to display

--- javascript/app.js
+++ javascript/app.js
@@ -1,3 +1,3 @@
 "use strict";
 
-var app = angular.module("TheBibleApp", ["ngRoute"]);
+var app = angular.module("TheBibleApp", ["ngRoute", "ui.bootstrap"]);
---
+++ javascript/filters/filters.js
@@ -0,0 +1,7 @@
+"use strict";
+
+app.filter('offset', function() {
+    return function(input, start) {
+        return input.slice(start);
+    };
+});
