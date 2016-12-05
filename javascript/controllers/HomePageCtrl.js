ad2797bedfd880290124aac0c0f64643e337b9d1
justin leggett <justin@codingrookie.com>
2016-12-05 16:04:51 -0600

    deleted ModalCtrl and overwrote NavCtrl to include all the infomation for the modal and the navigation bar.

--- javascript/controllers/HomePageCtrl.js
+++ javascript/controllers/HomePageCtrl.js
@@ -2,18 +2,6 @@
 
 app.controller("HomePageCtrl", function($scope, $location) {
 
-    $scope.login = () => {
-        console.log("Login Working!!!!!!");
-        // Login Modal goes here
-    };
 
-    $scope.register = () => {
-        console.log("Register Working!!!!!!");
-        // Register Modal goes here
-    };
 
-    $scope.readthebible = () => {
-        console.log("readthebible Working!!!!!!");
-        $location.url("/readbible");
-    };
 });
--- javascript/controllers/NavCtrl.js
+++ javascript/controllers/NavCtrl.js
@@ -1,9 +1,40 @@
 "use strict";
 
-app.controller("NavCtrl", ($scope) => {
-    $scope.navItems = [{name: "Logout", url: "#/logout"},
-    {name:"New Board", url: "#/boards/new"},
-    {name:"My Boards", url: "#/boards/list"},
-    {name:"New Pin", url: "#/pins/new"},
-    {name:"All Pins", url: "#/allpins/list"}];
+app.controller('NavCtrl', function($scope, $location, ModalService) {
+
+    $scope.login = () => {
+        console.log("Login Working!!!!!!");
+        // Login Modal goes here
+    };
+
+    $scope.register = () => {
+        console.log("Register Working!!!!!!");
+        // Register Modal goes here
+    };
+
+    $scope.readthebible = () => {
+        console.log("readthebible Working!!!!!!");
+        $location.url("/readbible");
+    };
+
+    $scope.show = function() {
+        ModalService.showModal({
+            templateUrl: './partials/modal.html',
+            controller: "ModalController"
+        }).then(function(modal) {
+            modal.element.modal();
+            modal.close.then(function(result) {
+                $scope.message = "You said " + result;
+            });
+        });
+    };
+
+});
+
+app.controller('ModalController', function($scope, close) {
+
+    $scope.close = function(result) {
+        close(result, 500); // close, but give 500ms for bootstrap to animate
+    };
+
 });
