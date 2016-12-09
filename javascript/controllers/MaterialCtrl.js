a5bbcfed33faf35e081afcdc90a4068cd775fa8f
justin leggett <justin@codingrookie.com>
2016-12-09 16:32:59 -0600

    trying to get materialize working

--- javascript/controllers/MaterialCtrl.js
+++ javascript/controllers/MaterialCtrl.js
@@ -2,39 +2,50 @@
 
 // var app = angular.module('TheBibleApp', ['ngMaterial']);
 
-app.controller('AppCtrl', ['$scope', '$mdSidenav', function($scope, $mdSidenav){
-  $scope.toggleSidenav = function(menuId) {
-    $mdSidenav(menuId).toggle();
-  };
+app.controller('AppCtrl', ['$scope', '$mdSidenav', 'BibleFactory', function($scope, $mdSidenav, BibleFactory){
+    $scope.toggleSidenav = function(menuId) {
+        $mdSidenav(menuId).toggle();
+    };
 
-  $scope.firstName="Paul";
+    $scope.bibles= {};
+    $scope.firstName="Paul";
 
-  $scope.toggleExpansion = function toggleExpansion(sectionId){
-    var sectionToToggle = $("#"+sectionId);
+    $scope.toggleExpansion = function toggleExpansion(sectionId){
+        var sectionToToggle = $("#"+sectionId);
     if(sectionToToggle.css("display")==="none"){
-      $scope.expandSection(sectionToToggle);
+        $scope.expandSection(sectionToToggle);
     } else {
-      $scope.collapseSection(sectionToToggle);
+        $scope.collapseSection(sectionToToggle);
     }
-  };
+    };
 
-  $scope.collapseSectionById = function collapseSectionById(sectionId){
-    var sectionToCollapse = $("#"+sectionId);
+    $scope.collapseSectionById = function collapseSectionById(sectionId){
+        var sectionToCollapse = $("#"+sectionId);
     sectionToCollapse.css("display","none");
 
-  };
+    };
 
-  $scope.collapseSection = function collapseSection(section){
-    section.css("display","none");
-  };
+    $scope.collapseSection = function collapseSection(section){
+        section.css("display","none");
+    };
 
-  $scope.expandSectionById = function expandSectionById(sectionId){
-    var sectionToExpand = $("#"+sectionId);
-    sectionToExpand.css("display", "inline");
-  };
+    $scope.expandSectionById = function expandSectionById(sectionId){
+        var sectionToExpand = $("#"+sectionId);
+        sectionToExpand.css("display", "inline");
+    };
 
-  $scope.expandSection = function expandSection(section){
+    $scope.expandSection = function expandSection(section){
     section.css("display", "inline");
-  };
+    };
+
+    // copy and paste from ReadBibleCtrl
+    let getKidsBible = () => {
+        BibleFactory.getKidsBible().then((response) => {
+            $scope.bibles = response;
+            console.log(response);
+        });
+    };
+    // populate array with all verses from db
+    getKidsBible();
 
 }]);
