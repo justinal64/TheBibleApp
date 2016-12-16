03594e7f761f3206f1c91f83a241e974d2e1139f
justin leggett <justin@codingrookie.com>
2016-12-16 15:26:30 -0600

    displaying text on page.

--- javascript/controllers/DBPCtrl.js
+++ javascript/controllers/DBPCtrl.js
@@ -5,6 +5,13 @@ app.controller("DBPCtrl", function($scope, DBPFactory) {
     $scope.chapters = {};
     $scope.bookSelected = {};
     $scope.showchapters = false;
+    $scope.adultbible = false;
+    $scope.book = "";
+    $scope.verses = {};
+    // Used to determine if it is the old or new testament
+    let NT = "ENGNASN2ET";
+    let OT = "ENGNASO2ET";
+    let testament = "";
 
 
     $scope.getBooksOfBible = () => {
@@ -30,23 +37,30 @@ app.controller("DBPCtrl", function($scope, DBPFactory) {
             if($scope.booksOfBibles[key].book_id === book_id) {
                 $scope.chapters = $scope.booksOfBibles[key].chapters;
                 $scope.showchapters = true;
+                $scope.book = book_id;
+                // determine weather it's old or new testament
+                if($scope.booksOfBibles[key].book_order <= 39) {
+                    testament = OT;
+                } else {
+                    testament = NT;
+                }
             }
         });
     };
 
+    $scope.chapterLookup = (chapter_id) => {
+        console.log("chapter_id = ", chapter_id);
+        console.log("$scope.book =", $scope.book);
+        console.log("testamnet = ", testament);
+        DBPFactory.getVerse($scope.book, chapter_id, testament).then((response) => {
+            $scope.verses = response;
+            $scope.adultbible = true;
+            console.log($scope.verses);
+        });
+        // clear the values after looked up
+        $scope.book = "";
+        testament = "";
+
+    };
+
 });
-
-                // let pins = [];
-                // Object.keys(response).forEach((key) => {
-                //     response[key].id = key;
-                //     pins.push(response[key]);
-                // });
-                // resolve(pins);
-
-
-
-// var a = ["a", "b", "c"];
-
-// a.forEach(function(element) {
-//     console.log(element);
-// });
--- javascript/factories/DBPFactory.js
+++ javascript/factories/DBPFactory.js
@@ -2,9 +2,6 @@
 
 app.factory("DBPFactory", function($q, $http) {
 
-    let NT = "ENGNASN2ET";
-    let OT = "ENGNASO2ET";
-
     // dam_id for NT ENGNASN2ET
     // This dam_id is pulling an NT english version of NAS New American Standard Bible
     var getBible = function() {
@@ -23,19 +20,19 @@ app.factory("DBPFactory", function($q, $http) {
 
     // dam_id for OT ENGNASO2ET
     // This dam_id is pulling an OT english version of NAS New American Standard Bible
-    var getOTBible = function() {
-        return $q((resolve, reject) => {
-            // $http.get(`${FIREBASE_CONFIG.databaseURL}/biblestories.json`)
-            $http.get(`http://dbt.io/library/bookorder?key=c0c769e931f78307a6c1c65cc5bd1d8c&dam_id=${OT}&v=2`)
-            // http://dbt.io/library/bookorder?key=c0c769e931f78307a6c1c65cc5bd1d8c&dam_id=ENGNASN2ET&v=2
-            .success((response) => {
-                resolve(response);
-            })
-            .error((errorResponse) => {
-                reject(errorResponse);
-            });
-        });
-    };
+    // var getOTBible = function() {
+    //     return $q((resolve, reject) => {
+    //         // $http.get(`${FIREBASE_CONFIG.databaseURL}/biblestories.json`)
+    //         $http.get(`http://dbt.io/library/bookorder?key=c0c769e931f78307a6c1c65cc5bd1d8c&dam_id=${OT}&v=2`)
+    //         // http://dbt.io/library/bookorder?key=c0c769e931f78307a6c1c65cc5bd1d8c&dam_id=ENGNASN2ET&v=2
+    //         .success((response) => {
+    //             resolve(response);
+    //         })
+    //         .error((errorResponse) => {
+    //             reject(errorResponse);
+    //         });
+    //     });
+    // };
 
     var getChapters = function(book_id) {
         return $q((resolve, reject) => {
@@ -49,9 +46,9 @@ app.factory("DBPFactory", function($q, $http) {
         });
     };
 
-    var getVerse = function(book_id, chapter_id) {
+    var getVerse = function(book_id, chapter_id, testament) {
         return $q((resolve, reject) => {
-            $http.get(`http://dbt.io/text/verse?key=c0c769e931f78307a6c1c65cc5bd1d8c&dam_id=ENGNASN2ET&book_id=${book_id}&chapter_id=${chapter_id}&v=2`)
+            $http.get(`http://dbt.io/text/verse?key=c0c769e931f78307a6c1c65cc5bd1d8c&dam_id=${testament}&book_id=${book_id}&chapter_id=${chapter_id}&v=2`)
             .success((response) => {
                 resolve(response);
             })
@@ -60,12 +57,6 @@ app.factory("DBPFactory", function($q, $http) {
             });
         });
     };
-
-
-
-
-
-
 
 
     // URL request for chapters of a book
